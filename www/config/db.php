9b7398ac06834beff598a690ec9a319bccbfaa23
JinSeeng <sellia.nade@gmail.com>
2025-04-13 01:16:41 +0200

    Fix/ Resolve database connection issue
    - Updated `db.php` to correct the database connection parameters and logic
    - Removed `config.php` to avoid conflicts and streamline configuration management

---
+++ www/config/db.php
@@ -0,0 +1,32 @@
+<?php
+// config/db.php
+
+class Database {
+    private static $instance = null;
+    private $connection;
+    
+    private function __construct() {
+        $host = '127.0.0.1';
+        $port = '8889';
+        $dbname = 'pause_wifi';
+        $username = 'root';
+        $password = 'root';
+        $socket = '/Applications/MAMP/tmp/mysql/mysql.sock';
+
+        try {
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname;unix_socket=$socket";
+            $this->connection = new PDO($dsn, $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->exec("SET NAMES 'utf8'");
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données: " . $e->getMessage());
+        }
+    }
+    
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance->connection;
+    }
+}
