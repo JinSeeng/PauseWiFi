cef1d35b0aa73b64062077cd37f49235eead105c
JinSeeng <sellia.nade@gmail.com>
2025-04-18 02:46:01 +0200

    Add/ Email configuration using PHPMailer
    - Added config/mail.php to configure PHPMailer SMTP settings for sending emails (e.g., password recovery, contact form).
    - Email templates and logic connect to forgot-password.php and reset-password.php.

---
+++ www/config/mail.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => 'sandbox.smtp.mailtrap.io',
+    'username' => '97230c17855313',
+    'password' => 'df0281e92529ab',
+    'port' => 2525,
+    'encryption' => null,
+    'from' => 'no-reply@pausewifi.com',
+    'from_name' => 'Pause WiFi'
+];
